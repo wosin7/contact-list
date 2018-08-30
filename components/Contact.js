e40d432c8026ff3b3b13957c9e863c3a91fe5b91
przemek <wosin7@gmail.com>
2018-08-30 17:43:39 +0200

    kontakty

---
+++ components/Contact.js
@@ -0,0 +1,21 @@
+var Contact = React.createClass({
+    propTypes: {
+      item: React.PropTypes.object.isRequired,
+    },
+  
+    render: function() {
+      return (
+        React.createElement('div', {className: 'contactItem'},
+          React.createElement('img', {
+            className: 'contactImage',
+            src: 'https://via.placeholder.com/150x150'
+          }),
+          React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
+          React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
+          React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
+            this.props.item.email
+          )
+        )
+      )
+    },
+  }); 
